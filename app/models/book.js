c23c3f3b129bcebbf2c15102efbdafa83cad6436
Mior Luqmanulhakim <mior.luqmanulhakim@me-tech.com.my>
2017-06-19 05:13:35 +0800

    first commit

---
+++ app/models/book.js
@@ -0,0 +1,27 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+//book schema definition
+let BookSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    year: { type: Number, required: true },
+    pages: { type: Number, required: true, min: 1 },
+    createdAt: { type: Date, default: Date.now },    
+  }, 
+  { 
+    versionKey: false
+  }
+);
+
+BookSchema.pre('save', next => {
+  now = new Date();
+  if(!this.createdAt) {
+    this.createdAt = now;
+  }
+  next();
+});
+
+//Exports the BookSchema for use elsewhere.
+module.exports = mongoose.model('book', BookSchema);
