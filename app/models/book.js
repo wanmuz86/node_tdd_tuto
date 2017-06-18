d0298f0082f293ae1447fe4ceeedbf642fc628da
Mior Luqmanulhakim <mior.luqmanulhakim@me-tech.com.my>
2017-06-19 05:19:29 +0800

    add desc

--- app/models/book.js
+++ app/models/book.js
@@ -7,7 +7,7 @@ var BookSchema = new Schema(
     title: { type: String, required: true },
     author: { type: String, required: true },
     year: { type: Number, required: true },
-    description: {type: String}
+    description: {type: String},
     pages: { type: Number, required: true, min: 1 },
     createdAt: { type: Date, default: Date.now },    
   }, 
