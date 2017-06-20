6239d5af0fe66a159b90930d7ce0f5fc40c4c088
Mior Luqmanulhakim <mior.luqmanulhakim@me-tech.com.my>
2017-06-20 08:18:42 +0800

    add more test

--- test/book.js
+++ test/book.js
@@ -56,7 +56,6 @@ describe('Books', function(){
               done();
             });
       });
-
       it('it should POST a book ', function(done){
         var book = {
             title: "The Lord of the Rings",
@@ -81,5 +80,59 @@ describe('Books', function(){
 
   });
 
-
-});
+   describe('/GET/:id book', function(){
+      it('it should GET a book by the given id', function(done){
+        let book = new Book({ title: "The Lord of the Rings", author: "J.R.R. Tolkien", year: 1954, pages: 1170 });
+        book.save(function(err, book){
+            chai.request(server)
+            .get('/book/' + book.id)
+            .send(book)
+            .end(function(err, res) {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('title');
+                res.body.should.have.property('author');
+                res.body.should.have.property('pages');
+                res.body.should.have.property('year');
+                res.body.should.have.property('_id').eql(book.id);
+              done();
+            });
+        });
+        });
+    })
+      
+describe('/PUT/:id book', function()  {
+      it('it should UPDATE a book given the id', function(done) {
+        let book = new Book({title: "The Chronicles of Narnia", author: "C.S. Lewis", year: 1948, pages: 778})
+        book.save(function(err, book) {
+                chai.request(server)
+                .put('/book/' + book.id)
+                .send({title: "The Chronicles of Narnia", author: "C.S. Lewis", year: 1950, pages: 778})
+                .end(function(err, res) {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('message').eql('Book updated!');
+                    res.body.book.should.have.property('year').eql(1950);
+                  done();
+                });
+          });
+      });
+  });
+describe('/DELETE/:id book', function() {
+      it('it should DELETE a book given the id', function(done) {
+        let book = new Book({title: "The Chronicles of Narnia", author: "C.S. Lewis", year: 1948, pages: 778})
+        book.save(function(err, book){
+                chai.request(server)
+                .delete('/book/' + book.id)
+                .end(function(err, res) {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('message').eql('Book successfully deleted!');
+                    res.body.result.should.have.property('ok').eql(1);
+                    res.body.result.should.have.property('n').eql(1);
+                  done();
+                });
+          });
+      })
+    })
+})
